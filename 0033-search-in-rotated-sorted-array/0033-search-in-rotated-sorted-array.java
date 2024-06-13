641f5ebb4ad285e104490b5ca5117d4bc5dfef63
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-06-13 16:59:10 +0530

    Time: 0 ms (100%), Space: 41.9 MB (41.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,58 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-
-        int rPoint = -1;
-        int ans = -2;
         int l = 0, u = nums.length - 1;
-        for(int i = 0; i < nums.length - 1; i++)
-        {
-            if(nums[i] > nums[i+1])
-            {
-                rPoint = i;
-                break;
-            }
-        }
-
-        if(rPoint == -1)
-        {
-            ans = bs(nums, target, l, u);
-                return ans;
-        }
-        else
-        {
-            ans = bs(nums, target, 0, rPoint);
-            if(ans != -1)
-                return ans;
-            else if(ans == -1)
-            {
-                ans =  bs(nums, target, rPoint + 1, u);
-                return ans;
-            }
-                
-        }
-
-        return -1;
-    }
-
-    public int bs(int nums[], int target, int l, int u)
-    {
-        int ans = -1;
         while(l <= u)
+        {
+            int mid = (l+u)/2;
+            if(target == nums[mid])
+                return mid;
+            if(nums[0] <= nums[mid])
             {
-                int mid = (l+u)/2;
-                if(nums[mid] == target)
+                if(target >= nums[l] && target <= nums[mid])
                 {
-                    return mid;
+                    u = mid - 1;
                 }
-
-                if(target > nums[mid])
+                else
+                    l = mid + 1;
+            }
+            else
+            {
+                if(target >= nums[mid] && target <= nums[u])
                 {
                     l = mid + 1;
                 }
-
                 else
                     u = mid - 1;
             }
-        return ans;
+        }
+
+        return -1;
     }
 }
