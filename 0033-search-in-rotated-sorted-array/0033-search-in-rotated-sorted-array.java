43dc120da62bd5732593fd13b275ae0f99999fe9
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-06-13 09:23:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,11 +1,58 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++)
+
+        int rPoint = -1;
+        int ans = -2;
+        int l = 0, u = nums.length - 1;
+        for(int i = 0; i < nums.length - 1; i++)
         {
-            if(nums[i] == target)
-                return i; 
+            if(nums[i] > nums[i+1])
+            {
+                rPoint = i;
+                break;
+            }
+        }
+
+        if(rPoint == -1)
+        {
+            ans = bs(nums, target, l, u);
+                return ans;
+        }
+        else
+        {
+            ans = bs(nums, target, 0, rPoint);
+            if(ans != -1)
+                return ans;
+            else if(ans == -1)
+            {
+                ans =  bs(nums, target, rPoint, u);
+                return ans;
+            }
+                
         }
 
         return -1;
+    }
+
+    public int bs(int nums[], int target, int l, int u)
+    {
+        int ans = -1;
+        while(l <= u)
+            {
+                int mid = (l+u)/2;
+                if(nums[mid] == target)
+                {
+                    return mid;
+                }
+
+                if(target > nums[mid])
+                {
+                    l = mid + 1;
+                }
+
+                else
+                    u = mid - 1;
+            }
+        return ans;
     }
 }
