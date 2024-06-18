8a9c952054767cbc5adb9901cb3e2760b02af08d
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-06-18 18:29:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,12 +1,34 @@
 class Solution {
     //Brute Force
     public int singleNonDuplicate(int[] nums) {
-        int ans = nums[0];
-        for(int i = 1; i < nums.length; i++)
+        int l = 0, u = nums.length - 1;
+        int ans = -1;
+        if(nums.length == 1)
         {
-            ans = ans^nums[i];
+            return nums[0];
+        }
+        l++;
+        u--;
+
+        while(l <= u)
+        {
+            int mid = (l+u)/2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
+            {
+                return nums[mid];
+            }
+
+            if((mid % 2 == 0 && nums[mid] == nums[mid-1]) 
+            || (mid % 2 == 1 && nums[mid] == nums[mid+1]))
+            {
+                u = mid - 1;
+            }
+
+            else
+                l = mid + 1;
         }
 
-        return ans;
+        return -1;
     }
 }
