35f09d08725996ddd98fc3970c0d0d46b4e3bbf7
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-07-24 19:28:36 +0530

    Time: 1 ms (27.89%), Space: 44.5 MB (40.08%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,19 +11,48 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashMap<ListNode, Integer> map = new HashMap<>();
-        ListNode temp = head;
-        int count = 0;
-        while(temp != null)
+
+        //Space complexity 0(n)
+
+        // HashMap<ListNode, Integer> map = new HashMap<>();
+        // ListNode temp = head;
+        // int count = 0;
+        // while(temp != null)
+        // {
+        //     if(map.get(temp) != null)
+        //     {
+        //         return temp;
+        //     }
+        //     else
+        //     {
+        //         map.put(temp, count++);
+        //         temp = temp.next;
+        //     }
+        // }
+
+        // return null;
+
+        // Optimal space
+
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null)
         {
-            if(map.get(temp) != null)
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
             {
-                return temp;
-            }
-            else
-            {
-                map.put(temp, count++);
-                temp = temp.next;
+                if(slow == head)
+                    return slow;
+                slow = head;
+                while(fast != null && fast.next != null)
+                {
+                    slow = slow.next;
+                    fast = fast.next;
+                    if(slow == fast)
+                        return slow;
+                }
             }
         }
 
