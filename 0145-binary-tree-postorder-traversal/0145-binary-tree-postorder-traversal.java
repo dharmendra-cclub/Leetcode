ac89bf83b5caf5ebf56c49ab6ad410c984c9efbd
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-08-26 00:08:11 +0530

    Time: 0 ms (100%), Space: 41.5 MB (64.59%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,23 +13,54 @@
  *     }
  * }
  */
+
+//********************************** Recursive approach
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> li = new ArrayList<Integer>();
+//         postorder(root, li);
+//         return li;
+//     }
+
+//     public void postorder(TreeNode root, List<Integer> li)
+//     {
+//         if(root == null) return;
+
+//         if(root.left != null)
+//             postorder(root.left, li);
+
+//         if(root.right != null)
+//             postorder(root.right, li);
+        
+//         li.add(root.val);
+//     }
+// }
+
+//********************************** Iterative approach
+
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> li = new ArrayList<Integer>();
-        postorder(root, li);
+        Stack<TreeNode> stack1 = new Stack<TreeNode>();
+        Stack<TreeNode> stack2 = new Stack<TreeNode>();
+        List<Integer> li = new ArrayList<>();
+
+        if(root == null) return li;
+        stack1.push(root);
+
+        while(!stack1.isEmpty())
+        {
+           TreeNode node = stack1.pop();
+           stack2.push(node);
+           if(node.left != null) stack1.push(node.left);
+           if(node.right != null) stack1.push(node.right);
+        }
+
+        while(!stack2.isEmpty())
+        {
+            li.add(stack2.pop().val);
+        }
+
         return li;
-    }
-
-    public void postorder(TreeNode root, List<Integer> li)
-    {
-        if(root == null) return;
-
-        if(root.left != null)
-            postorder(root.left, li);
-
-        if(root.right != null)
-            postorder(root.right, li);
-        
-        li.add(root.val);
     }
 }
