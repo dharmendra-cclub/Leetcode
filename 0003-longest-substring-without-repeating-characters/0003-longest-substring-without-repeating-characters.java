8ae2af3d4364e59e21bcab904c1a804f5408c50e
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-09-09 23:07:25 +0530

    Time: 5 ms (87.02%), Space: 44.9 MB (26.96%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,20 +1,48 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int len = 0, maxLength = 0;
+        //Brute force
+
+        // int len = 0, maxLength = 0;
+        // for(int i = 0; i < s.length(); i++)
+        // {
+        //     int hash[] = new int[255];
+        //     Arrays.fill(hash, 0);
+        //     for(int j = i; j < s.length(); j++)
+        //     {
+        //         char c = s.charAt(j);
+        //         if(hash[(int)c] == 1) break;
+        //         len = j - i + 1;
+        //         maxLength = Math.max(len, maxLength);
+        //         hash[(int)c] = 1;
+        //     }
+        // }
+
+        // return maxLength;
+
+        //Optimal approach using two pointer/sliding window
+
+        int left = 0, right = 0;
+        int len = 0, maxLen = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        //map.put(s.charAt(right), 0);
         for(int i = 0; i < s.length(); i++)
         {
-            int hash[] = new int[255];
-            Arrays.fill(hash, 0);
-            for(int j = i; j < s.length(); j++)
+            if(!map.containsKey(s.charAt(i)))
             {
-                char c = s.charAt(j);
-                if(hash[(int)c] == 1) break;
-                len = j - i + 1;
-                maxLength = Math.max(len, maxLength);
-                hash[(int)c] = 1;
+                len = right - left + 1;
+                map.put(s.charAt(i), i);
             }
+            else
+            {
+                left = Math.max(map.get(s.charAt(i)) + 1, left);
+                len = right - left + 1;
+                map.put(s.charAt(i), i);
+            }
+
+            maxLen = Math.max(len, maxLen);
+            right++;
         }
 
-        return maxLength;
+        return maxLen;
     }
 }
