e0cf05b7c7bdb5c2dfde139a08647b81d1760fa4
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-09-12 23:09:18 +0530

    Time: 6 ms (11.83%), Space: 46.5 MB (90.21%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,17 +1,43 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int maxLen = 0;
+        //Brute force
+        // int maxLen = 0;
+        // for(int i = 0; i < nums.length; i++)
+        // {
+        //     int len = 0, noOfZeroes = 0;
+        //     for(int j = i; j < nums.length; j++)
+        //     {
+        //         if(nums[j] == 0)
+        //             noOfZeroes++;
+        //         if(noOfZeroes > k) break;
+        //         len++;
+        //         maxLen = Math.max(maxLen, len);
+        //     }
+        // }
+
+        // return maxLen;
+
+
+
+        //Optimal approach
+        ArrayList<Integer> li = new ArrayList<>();
         for(int i = 0; i < nums.length; i++)
+            if(nums[i] == 0)    li.add(i);
+
+        int left = 0, right = 0, len = 0, maxLen = 0, noOfZeroes = 0;
+        int j = 0;
+        while(right < nums.length)
         {
-            int len = 0, noOfZeroes = 0;
-            for(int j = i; j < nums.length; j++)
+            if(nums[right] == 0)    noOfZeroes++;
+            if(noOfZeroes > k)
             {
-                if(nums[j] == 0)
-                    noOfZeroes++;
-                if(noOfZeroes > k) break;
-                len++;
-                maxLen = Math.max(maxLen, len);
+                left = li.get(j) + 1;
+                noOfZeroes--;
+                j++;
             }
+            len = right - left + 1;
+            maxLen = Math.max(len, maxLen);
+            right++;
         }
 
         return maxLen;
