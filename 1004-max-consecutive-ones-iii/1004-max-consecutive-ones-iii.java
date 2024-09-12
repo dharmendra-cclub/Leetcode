7a66e641cb3c62c99b91bbd547f7ce5f9504b452
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-09-12 23:04:28 +0530

    Time: 2622 ms (5.01%), Space: 46.7 MB (80.96%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -3,14 +3,13 @@ class Solution {
         int maxLen = 0;
         for(int i = 0; i < nums.length; i++)
         {
-            int len = 0;
-            int p = k;
+            int len = 0, noOfZeroes = 0;
             for(int j = i; j < nums.length; j++)
             {
                 if(nums[j] == 0)
-                    p--;
-                if(p >= 0)
-                    len++;
+                    noOfZeroes++;
+                if(noOfZeroes > k) break;
+                len++;
                 maxLen = Math.max(maxLen, len);
             }
         }
