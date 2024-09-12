8b5c4ad3f0f660b1e17ea9c7213c4e2b99061e0e
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-09-12 23:32:08 +0530

    Time: 4 ms (26.92%), Space: 47.5 MB (7.59%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -19,21 +19,40 @@ class Solution {
 
 
 
-        //Optimal approach
-        ArrayList<Integer> li = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++)
-            if(nums[i] == 0)    li.add(i);
+        //My Optimal approach
 
+        // ArrayList<Integer> li = new ArrayList<>();
+        // for(int i = 0; i < nums.length; i++)
+        //     if(nums[i] == 0)    li.add(i);
+
+        // int left = 0, right = 0, len = 0, maxLen = 0, noOfZeroes = 0;
+        // int j = 0;
+        // while(right < nums.length)
+        // {
+        //     if(nums[right] == 0)    noOfZeroes++;
+        //     if(noOfZeroes > k)
+        //     {
+        //         left = li.get(j) + 1;
+        //         noOfZeroes--;
+        //         j++;
+        //     }
+        //     len = right - left + 1;
+        //     maxLen = Math.max(len, maxLen);
+        //     right++;
+        // }
+
+        // return maxLen;
+
+        //Strivers approach
         int left = 0, right = 0, len = 0, maxLen = 0, noOfZeroes = 0;
         int j = 0;
         while(right < nums.length)
         {
             if(nums[right] == 0)    noOfZeroes++;
-            if(noOfZeroes > k)
+            while(noOfZeroes > k)
             {
-                left = li.get(j) + 1;
-                noOfZeroes--;
-                j++;
+                if(nums[left] == 0) noOfZeroes--;
+                left++;
             }
             len = right - left + 1;
             maxLen = Math.max(len, maxLen);
