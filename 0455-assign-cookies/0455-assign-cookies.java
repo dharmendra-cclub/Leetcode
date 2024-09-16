0a2328da1298d42932f6f780094786febfe49f2b
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-09-16 23:42:27 +0530

    Time: 11 ms (19.48%), Space: 48.8 MB (5.35%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,22 +1,43 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        // if(s.length == 0)   return 0;
+        // int count = 0;
+        // Arrays.sort(g);
+        // Arrays.sort(s);
+        // ArrayList<Integer> li = new ArrayList<>();
+        // for(int i = 0; i < s.length; i++)
+        //     li.add(s[i]);
+        // Iterator<Integer> it = li.iterator();
+        // for(int i = 0; i < g.length; i++)
+        // {
+        //      while (it.hasNext()) {
+        //         int j = it.next();
+        //         if (g[i] <= j) {
+        //             count++;
+        //             break;
+        //         }
+        //         it.remove();
+        //     }
+        // }
+
+        // return count;
+
         if(s.length == 0)   return 0;
         int count = 0;
         Arrays.sort(g);
         Arrays.sort(s);
-        ArrayList<Integer> li = new ArrayList<>();
-        for(int i = 0; i < s.length; i++)
-            li.add(s[i]);
-        Iterator<Integer> it = li.iterator();
-        for(int i = 0; i < g.length; i++)
+        int l = 0, r = 0;
+        while(l < g.length && r < s.length)
         {
-             while (it.hasNext()) {
-                int j = it.next();
-                if (g[i] <= j) {
-                    count++;
-                    break;
-                }
-                it.remove();
+            while(r < s.length && g[l] > s[r])
+            {
+                r++;
+            }
+            if(r < s.length && g[l] <= s[r])
+            {
+                count++;
+                l++;
+                r++;
             }
         }
 
