8fc5e5db8b290882266c8cde4af3477e4d37e683
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-10-08 18:06:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,23 @@
 class Solution {
+    //Using library
+    // public double myPow(double x, int n) {
+    //     return Math.pow(x,n);
+    // }
+
+    //Using recursion
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double ans = (n > 0) ? calcPower(x , n) : 1/(calcPower(x , n*(-1)));
+        
+        return ans;
+    }
+    public double calcPower(double x, long n)
+    {
+        if(n == 0)
+            return 1;
+        if(n % 2 == 1)
+            return x * calcPower(x, n-1);
+        
+        else 
+            return calcPower(x * x, n/2);
     }
 }
