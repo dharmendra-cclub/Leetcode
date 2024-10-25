a6ecb12cee8ea7080c0767adaf95f5c74ef4f5aa
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-10-25 20:17:14 +0530

    Time: 0 ms (100%), Space: 41.1 MB (67.04%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,30 +1,50 @@
 
 class Solution {
     public boolean check(int[] nums) {
-        int a[] = nums.clone();
-        int length = nums.length;
-        boolean flag = true;
-        Arrays.sort(a);
-        for(int i = 0; i < length; i++)
+
+        for(int i = 0; i < nums.length - 1; i++)
         {
-            int lastElement = a[length-1];
-            for(int j = length - 1; j > 0; j--)
+            if(nums[i] > nums[i+1])
             {
-                a[j] = a[j-1];
-            }
-            a[0] = lastElement;
-            flag = true;
-            for(int k = 0; k < length; k++)
-            {
-                if(a[k] != nums[k])
+                i++;
+                while(i < nums.length - 1)
                 {
-                    flag = false;
-                    break;
+                    if(nums[i] > nums[i+1])
+                        return false;
+                    i++;
                 }
+                if(nums[nums.length - 1] <= nums[0])
+                    return true;
+                else
+                    return false;
             }
-            if(flag == true)
-            return flag;
         }
-        return flag;
+
+        return true;
+        // int a[] = nums.clone();
+        // int length = nums.length;
+        // boolean flag = true;
+        // Arrays.sort(a);
+        // for(int i = 0; i < length; i++)
+        // {
+        //     int lastElement = a[length-1];
+        //     for(int j = length - 1; j > 0; j--)
+        //     {
+        //         a[j] = a[j-1];
+        //     }
+        //     a[0] = lastElement;
+        //     flag = true;
+        //     for(int k = 0; k < length; k++)
+        //     {
+        //         if(a[k] != nums[k])
+        //         {
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag == true)
+        //     return flag;
+        // }
+        // return flag;
     }
 }
