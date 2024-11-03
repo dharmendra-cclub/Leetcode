4e98ea28ef6620c451b9d628e43571c22e1a6655
Dharmendra Singh Yadav <30904602+dharmendra-cclub@users.noreply.github.com>
2024-11-03 17:40:56 +0530

    Time: 7 ms (23.47%), Space: 76.5 MB (86.02%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,25 +1,39 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int size = nums.length;
-        int positives[] = new int[size/2];
-        int negatives[] = new int[size/2];
-        int k = 0, j = 0;
-        for(int i = 0; i < size; i++)
+        // int size = nums.length;
+        // int positives[] = new int[size/2];
+        // int negatives[] = new int[size/2];
+        // int k = 0, j = 0;
+        // for(int i = 0; i < size; i++)
+        // {
+        //     if(nums[i] > 0)
+        //     positives[k++] = nums[i];
+        //     else
+        //     negatives[j++] = nums[i];
+        // }
+        // k = 0;
+        // j = 0;
+        // while(k < size && j < size/2)
+        // {
+        //     nums[k++] = positives[j];
+        //     nums[k++] = negatives[j];
+        //     j++;
+        // }
+
+        // return nums;
+        int left = 0, right = 0;
+        int ans[] = new int[nums.length];
+        int k = 0;
+        for(int i = 0; i < nums.length/2; i++)
         {
-            if(nums[i] > 0)
-            positives[k++] = nums[i];
-            else
-            negatives[j++] = nums[i];
-        }
-        k = 0;
-        j = 0;
-        while(k < size && j < size/2)
-        {
-            nums[k++] = positives[j];
-            nums[k++] = negatives[j];
-            j++;
+            while(nums[left] < 0)
+                left++;
+            while(nums[right] > 0)
+                right++;
+            ans[k++] = nums[left++];
+            ans[k++] = nums[right++];
         }
 
-        return nums;
+        return ans;
     }
 }
